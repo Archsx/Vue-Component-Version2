380329f1b90d4467071ef445889e218d43f322b9
Archsx <871728578@qq.com>
2020-07-30 18:13:36 +0800

    添加Tabs相关组件

---
+++ src/Tab/Tabs.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tabs">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Tabs',
+  props:{
+    selected:{
+      type:String,
+      required:true
+    },
+    direction:{
+      type:String,
+      default:'horizon',
+      validator(value){
+        return ['horizon','vertical'].includes(value)
+      }
+    }
+  },
+  created() {
+    //this.$emit('update:selected','xxx')
+  },
+}
+</script>
+
+<style >
+
+</style>
---
+++ src/Tab/app.js
@@ -0,0 +1,24 @@
+import Vue from "vue"
+import Tabs from './Tabs.vue'
+import TabsBody from './TabsBody.vue'
+import TabsHead from './TabsHead.vue'
+import TabsItem from './TabsItem.vue'
+import TabsPane from './TabsPane.vue'
+
+
+
+new Vue({
+  el:"#app",
+  components:{
+    Tabs,
+    TabsBody,
+    TabsHead,
+    TabsItem,
+    TabsPane
+  },
+  data() {
+    return {
+      selectedTab:'sports'
+    }
+  },
+})
