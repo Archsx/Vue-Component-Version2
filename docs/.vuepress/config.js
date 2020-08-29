43b5d1e6563861bcab02524ac2aa0920f6febf1a
Archsx <871728578@qq.com>
2020-08-30 00:59:56 +0800

    修改vuepress设置，边栏默认展开

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,19 @@
 module.exports = {
+  base: "/Vue-Component-Version2/",
   title: "Vue Custom Component",
   description: "Just playing around",
   themeConfig: {
     sidebar: [
       {
-        title:'入门',
-        children:[
-          '/install/',
-          '/start/'
-        ]
+        title: "入门",
+        collapsable: false,
+        children: ["/install/", "/start/"],
       },
       {
-        title:'组件',
-        children:[
-          '/components/button.md'
-        ]
-      }
-
-
+        title: "组件",
+        collapsable: false,
+        children: ["/components/button.md"],
+      },
     ],
   },
 };
