ae4758fa96ca4dcc0e98e3b8b203142e254fd856
Archsx <871728578@qq.com>
2020-09-05 16:54:27 +0800

    创建简单省市区json数据

---
+++ assets/district/data.js
@@ -0,0 +1,29 @@
+let data = require("./db.json");
+let fs = require("fs");
+let path = require("path");
+const filePath = path.resolve(__dirname, "./data.json");
+
+
+let res = data;
+
+let indexTree = res.reduce((obj, ele) => {
+  let id = ele.id;
+  obj[id] = ele;
+  return obj;
+}, {});
+
+let result = res.reduce((arr, ele) => {
+  let pid = ele.pid;
+  let parentNode = indexTree[pid];
+  if (parentNode) {
+    parentNode.children
+      ? parentNode.children.push(ele)
+      : (parentNode.children = [ele]);
+  } else if (pid === 0) {
+    arr.push(ele);
+  }
+  return arr;
+}, []);
+
+
+fs.writeFileSync(filePath, JSON.stringify(result));
--- src/Cascader/Cascader.vue
+++ src/Cascader/Cascader.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="cascader">
-    <div class="trigger" @click="popoverVisible = !popoverVisible"></div>
+    <div class="trigger" @click="popoverVisible = !popoverVisible">
+      {{ result }}
+    </div>
     <div class="popover-wrapper" v-if="popoverVisible">
       <cascader-items
         :items="options"
         class="popover"
         :height="popoverHeight"
-        :selected = "selected"
+        :selected="selected"
         @update:selected="onUpdateSelected"
       ></cascader-items>
     </div>
@@ -20,6 +22,11 @@ export default {
   components: {
     CascaderItems,
   },
+  computed: {
+    result() {
+      return this.selected.map((ele) => ele.label).join(" / ");
+    },
+  },
   props: {
     options: {
       type: Array,
@@ -29,9 +36,9 @@ export default {
     },
     selected: {
       type: Array,
-      default:() => {
-        return []
-      }
+      default: () => {
+        return [];
+      },
     },
   },
   data() {
@@ -40,24 +47,49 @@ export default {
     };
   },
   methods: {
-    onUpdateSelected(item){
-      this.$emit('update:selected',item)
-    }
+    onUpdateSelected(item) {
+      this.$emit("update:selected", item);
+    },
   },
 };
 </script>
 
 <style scoped lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, PingFang SC,
+    Hiragino Sans GB, Helvetica Neue, Helvetica, Arial, sans-serif,
+    Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
+  font-variant: tabular-nums;
+  line-height: 1.5;
+  color: #314659;
+  font-size: 14px;
+}
+
 .cascader {
   position: relative;
+  height: 32px;
   .trigger {
-    border: 1px solid black;
-    height: 32px;
-    width: 100px;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, PingFang SC;
+    height: 100%;
+    border: 1px solid #d9d9d9;
+    display: inline-flex;
+    align-items: center;
+    min-width: 300px;
+    border-radius: 4px;
+    padding: 4px 11px;
+    line-height: 1.5;
+    color: rgba(0, 0, 0, 0.65);
   }
   .popover-wrapper {
     position: absolute;
-    top: 100%;
+    font-size: 14px;
+    top: calc(100% + 2px);
     left: 0;
     background-color: #fff;
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
